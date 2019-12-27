d67c9953eedd133518fe5f5d75ec0250ab490750
Daryl Corbin <corbindac@gmail.com>
2019-12-27 23:16:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
 def addition(num1, num2)
-
+sum = num2 + num1
 end
+addition(3,5)
 
 def subtraction(num1, num2)
-
+  sum = num2 - num1
 end
 
 def division(num1, num2)
-
+sum = num2 / num1
 end
 
 def multiplication(num1, num2)
-
+sum = num2 * num1
 end
 
 def modulo(num1, num2)
-
+sum = num1 % num2
 end
 
 def square_root(num)
-
+sum = num1 + num2
 end
