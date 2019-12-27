50898881ec4aca425a8cd56306da60e1b746ee59
Daryl Corbin <corbindac@gmail.com>
2019-12-27 23:34:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,29 +1,23 @@
 def addition(num1, num2)
 sum = num2 + num1
 end
-addition(3,5)
 
 def subtraction(num1, num2)
   sum = num2 - num1
 end
-subtraction(15.class,50.class)
 
 def division(num1, num2)
 sum = num2 / num1
 end
-division(50,2)
 
 def multiplication(num1, num2)
 sum = num1 * num2
 end
-multiplication(4,9)
 
 def modulo(num1, num2)
 sum = num2 % num1
 end
-modulo(25,100)
 
 def square_root(num)
 sum = Math.sin(19)
 end
-square_root(19)
